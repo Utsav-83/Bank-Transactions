bd7f95c1f47d54431ccd3a6ccbc9b20eb128c859
Utsav-83 <utsav8391@gmail.com>
2025-10-04 11:00:50 +0530

    bank transctions

---
+++ bank.py
@@ -0,0 +1,7 @@
+initial_balance = float(input("Enter initial balance: "))
+
+deposit = float(input("Enter amount to deposit: "))
+
+updated_balance = initial_balance + deposit
+
+print("Updated balance after deposit is:", updated_balance)
